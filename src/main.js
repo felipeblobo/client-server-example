9f4bee05c1126bb5e138b7ee9b0b6e6a1942fdcd
Felipe Lôbo <69439442+felipeblobo@users.noreply.github.com>
2021-07-13 00:33:37 -0300

    pequenas rotas para autenticacao

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createServer } from 'http';
+import { readFile } from 'fs';
+import { resolve } from 'path';
+import { parse } from 'querystring';
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8001;
 const hostname = process.env.HOSTNAME || '127.0.0.1';
@@ -13,6 +16,51 @@ const server = createServer((req, res) => {
           status: 'Ok'
         }));
         res.end();
+        break;
+      }
+      case '/sign-in': {
+        const path = resolve(__dirname, './pages/sign-in.html');
+        readFile(path, (error, file) => {
+          if (error) {
+            res.writeHead(500, 'Erro ao processar arquivo!');
+            res.end();
+            return;
+          }
+          res.writeHead(200);
+          res.write(file);
+          res.end();
+        })
+        break;
+      }
+
+      case '/home': {
+        const path = resolve(__dirname, './pages/home.html');
+        readFile(path, (error, file) => {
+          if (error) {
+            res.writeHead(500, 'Erro ao processar arquivo!');
+            res.end();
+            return;
+          }
+          res.writeHead(200);
+          res.write(file);
+          res.end();
+
+        })
+        break;
+      }
+
+      case '/authenticate': {
+        let data = '';
+        req.on('data', (chunk) => {
+          data += chunk;
+        });
+        req.on('end', () => {
+          const params = parse(data).JSON
+          res.writeHead(301, {
+            Location: '/home',
+          })
+          res.end();
+        })
         break;
       }
     default: {
